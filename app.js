3af0e2c93abfd44f2e90b5067e2a1bb4f16607ff
Chitvan Gupta <chitvangupta01@gmail.com>
2023-09-10 00:13:49 +0530

    first

---
+++ app.js
@@ -0,0 +1,23 @@
+let btn = document.querySelector("button");
+let inp = document.querySelector("input");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click", function () {
+  let item = document.createElement("li");
+  item.innerText = inp.value;
+
+  let delBtn = document.createElement("button");
+  delBtn.innerText = "Delete";
+  delBtn.classList.add("delete");
+  item.appendChild(delBtn);
+
+  ul.appendChild(item);
+  inp.value = "";
+});
+
+ul.addEventListener("click", function (event) {
+  if (event.target.nodeName == "BUTTON") {
+    let listItem = event.target.parentElement;
+    listItem.remove();
+  }
+});
